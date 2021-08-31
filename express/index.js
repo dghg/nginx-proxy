e3e25075f3a4c8ab10603400012ce46fddbe6d98
dghg <dh@uos.ac.kr>
2021-08-31 19:22:14 +0900

    config update

--- express/index.js
+++ express/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+app.set('trust proxy', true);
+
 app.get('/', (req, res) => {
   var json = {
 	  host: req.hostname,
 	  ip: req.ips,
 	  header: req.rawHeaders,
 	  body: req.body,
+	  protocol: req.protocol,
 	  cookies: req.cookies,
+	  res: res.rawHeaders,
   };
+  res.cookie('testCookie', 'testCookieValue', { maxAge: 900000, httpOnly: true });
   res.send(json);
 })
 
